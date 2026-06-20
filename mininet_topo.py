f89584892e4a8aee87ddaf596cdb262265cdeab7
Cuneyt Terzi <civilator@hotmail.com>
2026-06-20 14:06:37 -0400

    update comments

--- mininet_topo.py
+++ mininet_topo.py
@@ -26,34 +26,34 @@ from mininet.log import setLogLevel, info
 def create():
     net = Mininet(controller=RemoteController, switch=OVSKernelSwitch)
 
-    info('*** Adding controller\n')
+    info('--controller\n')
     net.addController('c0', controller=RemoteController,
                       ip='127.0.0.1', port=6655)
 
-    info('*** Adding switch\n')
+    info('--switch\n')
     s1 = net.addSwitch('s1', protocols='OpenFlow10')
 
-    info('*** Adding hosts\n')
+    info('--hosts\n')
     h1 = net.addHost('h1', ip='192.168.2.10/24', mac='00:00:00:00:00:01')
     h2 = net.addHost('h2', ip='192.168.2.20/24', mac='00:00:00:00:00:02')
     h3 = net.addHost('h3', ip='192.168.2.30/24', mac='00:00:00:00:00:03')
     h4 = net.addHost('h4', ip='192.168.2.40/24', mac='00:00:00:00:00:04')
 
-    info('*** Creating links\n')
+    info('--links\n')
     net.addLink(h1, s1)
     net.addLink(h2, s1)
     net.addLink(h3, s1)
     net.addLink(h4, s1)
 
-    info('*** Starting network\n')
+    info('--starting network\n')
     net.start()
 
-    info('*** Testing connectivity\n')
+    info('--connectivity\n')
     net.pingAll()
 
     CLI(net)
 
-    info('*** Stopping network\n')
+    info('--stopping network\n')
     net.stop()
 
 
